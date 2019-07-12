8a12998514d61b6775b22cb74692d84ed3228a7d
Quan Pham <doraemonfanclub@gmail.com>
2019-07-12 19:43:38 +0700

    Convert the plugin to Activity-based

---
+++ android/src/main/kotlin/com/clipsub/flutter_aztec_editor/FlutterAztecEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.clipsub.flutter_aztec_editor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlutterAztecEditorActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_flutter_aztec_editor)
+  }
+}
